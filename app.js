0d42381182e1cfce32ece78b8117428c6c41b843
iiTzShaa <psnsharon@gmail.com>
2025-05-31 11:40:39 +0300

    Add app.js and utils folder

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./utils/db");
+const costRoutes = require("./routes/costRoutes");
+ const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use("/api", costRoutes);
+app.use("/api", userRoutes);
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
---
+++ utils/db.js
@@ -0,0 +1,22 @@
+/**
+ * Establishes a connection to the MongoDB database.
+ * Utilizes the connection string from the environment variable `MONGO_URI`.
+ * @async
+ * @function connectDB
+ * @returns {Promise<void>} Logs the status of the connection or exits the process on failure.
+ */
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
